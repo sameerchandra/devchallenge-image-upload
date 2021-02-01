46f63f40aacc163d58cbe586ad266da963433356
sameerchandra <scsameerchandra@gmail.com>
2021-02-01 12:20:54 +0530

    Changed port number

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const path = require('path');
 const app = express()
 
-var port = proces.env.PORt || 5000
+var port = process.env.PORT || 5000
 
 app.use(express.static(path.join(__dirname, 'client/build')));
 
